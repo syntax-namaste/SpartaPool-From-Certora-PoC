5401cbf4f3f2587db1c46fb9117ab356552f4a8e
syntax-namaste <syntax.namaste@gmail.com>
2023-02-02 09:02:01 +0530

    remove unused import

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-tracer");
-const tdly = require("@tenderly/hardhat-tenderly");
 // tdly.setup(); // comment this if you want to run on local 
 
 /** @type import('hardhat/config').HardhatUserConfig */
