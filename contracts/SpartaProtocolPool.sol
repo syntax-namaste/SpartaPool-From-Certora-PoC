0d086f2a833a0ab834f5ca0cd125affc101c8eeb
syntax-salaam <syntax.namaste@gmail.com>
2023-02-03 03:04:59 +0000

    SxNa comments

--- contracts/SpartaProtocolPool.sol
+++ contracts/SpartaProtocolPool.sol
@@ -3,15 +3,15 @@ pragma solidity ^0.8.4;
 
 import "./ERC20.sol";
 
-/* 
+/*
  * From Spartan Protocol medium post:
  *
- * The Spartan Protocol provides incentives to enable deep capital formation in liquidity pools, 
+ * The Spartan Protocol provides incentives to enable deep capital formation in liquidity pools,
  * with safe and sustainable creation of synthetic assets.
  *
- * 
+ *
  * A simplified explanation of the platform:
- * 
+ *
  * This is an implementation of a liquidity pool.
  * A creator of the pool specify 2 tokens to create a liquidity pool.
  * The creator then transfer assets to the pool as he/she wishes and initialize the pool.
@@ -19,7 +19,7 @@ import "./ERC20.sol";
  * The owner receives 100,000 LP token in exchange for his/her liquidity.
  *
  * Any user can add/remove liquidity to the pool in exchange for LP tokens.
- * 
+ *
  *
  */
 contract Pool is ERC20 {
@@ -37,16 +37,16 @@ contract Pool is ERC20 {
         token1 = _token1;
         require(token0 != token1, "can't be same tokens");
         owner = msg.sender;
-    }    
-    
+    }
+
     // initializing the pool according to the amount of tokens sent by the creator.
     // the creator receives 100,000 LP tokens (shares) in exchange.
     function init_pool() public {
         require(msg.sender == owner);
         token0Amount = IERC20(token0).balanceOf(address(this));
         token1Amount = IERC20(token1).balanceOf(address(this));
-        
-        K = token0Amount * token1Amount; 
+
+        K = token0Amount * token1Amount;
         balances[owner] = 100000;
         total = balances[owner];
     }
@@ -61,8 +61,8 @@ contract Pool is ERC20 {
         uint units = mint(msg.sender, added0, added1);
         uint LP_total_supply = total;
 
-        K = (K / (LP_total_supply-units)) * (LP_total_supply);
-        
+        K = (K / (LP_total_supply - units)) * (LP_total_supply);
+
         sync();
         return units;
     }
@@ -72,23 +72,31 @@ contract Pool is ERC20 {
         // sync();      // add sync() here to solve the bug
         burn(msg.sender, LP_tokens);
         uint LP_total_supply = total;
-        K = K * LP_total_supply / (LP_total_supply + LP_tokens);
+        K = (K * LP_total_supply) / (LP_total_supply + LP_tokens);
     }
-    
+
     // Automated Market Maker (AMM) - calculating the exchange rate of a desired exchange
     function swap(address from_token) public {
-        require((from_token == token0 || from_token == token1), "Must be toekn0 or token1");
-        address to_token = from_token == token0 ?  token1 : token0;
-   
+        require(
+            (from_token == token0 || from_token == token1),
+            "Must be toekn0 or token1"
+        );
+        address to_token = from_token == token0 ? token1 : token0;
+
         // get balance for the token_from in user's account and transfer it to the pool
         uint from_token_balance = IERC20(from_token).balanceOf(msg.sender);
-        IERC20(from_token).transferFrom(msg.sender, address(this), from_token_balance); // from customer to pool
+        IERC20(from_token).transferFrom(
+            msg.sender,
+            address(this),
+            from_token_balance
+        ); // from customer to pool
 
         // DONT UNDERSTAND
-        uint to_token_send = IERC20(from_token).balanceOf(msg.sender) * IERC20(to_token).balanceOf(msg.sender) - K;
+        uint to_token_send = IERC20(from_token).balanceOf(msg.sender) *
+            IERC20(to_token).balanceOf(msg.sender) -
+            K;
         IERC20(to_token).transfer(msg.sender, to_token_send); // From the pool to the customer
         sync();
-        
     }
 
     function getContractAddress() public view returns (address) {
@@ -107,16 +115,20 @@ contract Pool is ERC20 {
         token0Amount = IERC20(token0).balanceOf(address(this));
         token1Amount = IERC20(token1).balanceOf(address(this));
     }
-    
+
     // mints LP tokens to user
-    function mint(address user, uint amount0, uint amount1) internal returns (uint){
+    function mint(
+        address user,
+        uint amount0,
+        uint amount1
+    ) internal returns (uint) {
         uint totalBalance0 = IERC20(token0).balanceOf(address(this));
         uint totalBalance1 = IERC20(token1).balanceOf(address(this));
 
-        // minting deserved from supplying token 0 or 1 is the portion of the user's 
+        // minting deserved from supplying token 0 or 1 is the portion of the user's
         // supplied liquidity out of the total assets in the pool before the addition.
-        uint mint_0 = total * amount0 / (totalBalance0-amount0);
-        uint mint_1 = total * amount1 / (totalBalance1-amount1);
+        uint mint_0 = (total * amount0) / (totalBalance0 - amount0);
+        uint mint_1 = (total * amount1) / (totalBalance1 - amount1);
 
         // the liquidity providers are being incentivised to supply liquidity based on the existing ratio.
         // if the user choose to deviate from the ratio, they will get LP tokens according to the lower of the 2 amounts.
@@ -128,13 +140,15 @@ contract Pool is ERC20 {
         return to_mint;
     }
 
-    function burn(address user, uint LP_tokens) internal  {
+    function burn(address user, uint LP_tokens) internal {
         require(balances[user] >= LP_tokens);
         // calculates the amount of token0 or token1 that the user is eligble to receive.
         // it is the portion of his/her LP tokens out of the total.
-        uint pay_in_0 = LP_tokens * IERC20(token0).balanceOf(address(this)) / total;
-        uint pay_in_1 = LP_tokens * IERC20(token1).balanceOf(address(this)) / total;
-        
+        uint pay_in_0 = (LP_tokens * IERC20(token0).balanceOf(address(this))) /
+            total;
+        uint pay_in_1 = (LP_tokens * IERC20(token1).balanceOf(address(this))) /
+            total;
+
         // updates the user's, total, and contract amount of LP token count
         balances[user] -= LP_tokens;
         total -= LP_tokens;
@@ -144,26 +158,43 @@ contract Pool is ERC20 {
         // transfer liquidity token to user
         IERC20(token0).transfer(user, pay_in_0);
         IERC20(token1).transfer(user, pay_in_1);
-    }    
+    }
 
-    // HC: utility functions
-    function getTokenBalance(address token_, address user) public view returns (uint256) {
-        return IERC20(token_).balanceOf(user);
+    // SxNa: utility functions
+    function getTokenBalance(
+        address token_,
+        address user
+    ) public view returns (uint256) {
+        return IERC20(token_).balanceOf(user); // actual token balance
     }
-    function getTokenBalance__(address token_, address user) external { 
-        emit log("actual token balance", token_, user, IERC20(token_).balanceOf(user));
+
+    function getTokenBalance__(address token_, address user) external {
+        emit log(
+            "actual token balance",
+            token_,
+            user,
+            IERC20(token_).balanceOf(user)
+        );
     }
-    function recordedTokenBalance__(address token_) external { 
-        uint256 tokenAmount = token_ == token0? token0Amount : token1Amount;
+
+    // pool's recorded token balance
+    function recordedTokenBalance( 
+        address token_
+    ) public view returns (uint256) {
+        uint256 tokenAmount = token_ == token0 ? token0Amount : token1Amount;
+        return tokenAmount;
+    }
+
+    function recordedTokenBalance__(address token_) external {
+        uint256 tokenAmount = token_ == token0 ? token0Amount : token1Amount;
         emit log("recorded token balance in pool", token_, tokenAmount);
     }
 
     function getK() public view returns (uint256) {
         return K;
     }
+
     function getK__() external {
         emit log("K", K);
     }
-
-
 }
--- specs/Sparta.spec
+++ specs/Sparta.spec
@@ -2,39 +2,63 @@ methods {
     getContractAddress() returns address envfree
     getToken0DepositAddress() returns address envfree
     getToken1DepositAddress() returns address envfree
-    token0Balance(address) returns uint256 envfree
-    token1Balance(address) returns uint256 envfree
+    getTokenBalance(address, address) returns uint256 envfree
+    recordedTokenBalance(address) returns uint256 envfree
     owner() returns address envfree
     token0Amount() returns uint256 envfree
     token1Amount() returns uint256 envfree
     K() returns uint256 envfree
 
     // ERC20 functions
-    balanceOf(address) returns uint256 => DISPATCHER(true)
+    balanceOf(address) returns uint256 envfree => DISPATCHER(true)
 }
 
-rule addLiqDecreasesBalanceIncreasesLPForUser(env e, method f, address user) {
+rule addLiqDecreasesBalanceIncreasesLPForUser(env e0, method f) {
 
     // require (getToken0DepositAddress() == getToken1DepositAddress());
-    require (user == owner() && user != currentContract);
-    init_pool(e);
+    // require (user == owner() && user != currentContract);
+    require (e0.msg.sender == owner() && e0.msg.sender != currentContract);
+    init_pool(e0);
 
-    uint256 balanceToken0Before = token0Balance(user);
-    uint256 balanceToken1Before = token1Balance(user);
+    env e;
+    uint256 balanceToken0Before = getTokenBalance(getToken0DepositAddress(), e.msg.sender);
+    uint256 balanceToken1Before = getTokenBalance(getToken1DepositAddress(), e.msg.sender);
 
-    uint contractToken0BalanceBefore = token0Balance(currentContract);
-    uint contractToken1BalanceBefore = token1Balance(currentContract);
+    uint contractToken0BalanceBefore = getTokenBalance(getToken0DepositAddress(), currentContract);
+    uint contractToken1BalanceBefore = getTokenBalance(getToken1DepositAddress(), currentContract);
     
-    add_liquidity(e);
+    add_liquidity(e); 
+    
+    uint256 balanceToken0After = getTokenBalance(getToken0DepositAddress(), e.msg.sender);
+    uint256 balanceToken1After = getTokenBalance(getToken1DepositAddress(), e.msg.sender);
+
+    uint contractToken0BalanceAfter = getTokenBalance(getToken0DepositAddress(), currentContract);
+    uint contractToken1BalanceAfter = getTokenBalance(getToken1DepositAddress(), currentContract);
+
+    assert (balanceToken0Before >= balanceToken0After && balanceToken1Before >= balanceToken1After), 
+        "user's token balance increased after add_liq()";
+    assert (contractToken0BalanceBefore <= contractToken0BalanceAfter && 
+        contractToken1BalanceBefore >= contractToken1BalanceAfter), "contracts's token balance decreased after add_liq()";
+}
+
+rule actualAndRecordedPoolBalanceEqual(env e0, method f) {
+    
+    require (e0.msg.sender == owner() && e0.msg.sender != currentContract);
+    require (getToken0DepositAddress() == getToken1DepositAddress());
+    init_pool(e0);
+
+    uint contractToken0BalanceBefore = getTokenBalance(getToken0DepositAddress(), currentContract);
+    uint recordedToken0BalanceBefore = recordedTokenBalance(getToken0DepositAddress());
+    require contractToken0BalanceBefore == recordedToken0BalanceBefore;
+    
+    env e;
     // calldataarg args;
     // f(e, args);
+    uint256 LPTokens;
+    remove_liquidity(e, LPTokens);
     
-    uint256 balanceToken0After = token0Balance(user);
-    uint256 balanceToken1After = token1Balance(user);
+    uint contractToken0BalanceAfter = getTokenBalance(getToken0DepositAddress(), currentContract);
+    uint recordedToken0BalanceAfter = recordedTokenBalance(getToken0DepositAddress());
 
-    uint contractToken0BalanceAfter = token0Balance(currentContract);
-    uint contractToken1BalanceAfter = token1Balance(currentContract);
-
-    assert (balanceToken0Before >= balanceToken0After && balanceToken1Before >= balanceToken1After), "user's token balance increased after add_liq()";
-    assert (contractToken0BalanceBefore <= contractToken0BalanceAfter && contractToken1BalanceBefore >= contractToken1BalanceAfter), "contracts's token balance decreased after add_liq()";
+    assert (contractToken0BalanceAfter == recordedToken0BalanceAfter), "pool balance mismatch";
 }
